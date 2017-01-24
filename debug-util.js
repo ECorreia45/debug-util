7f279818aa59750b63a9bb40ff4473464f4b2168
ecorreia design <el4591@hotmail.com>
2017-01-24 17:23:59 -0500

    log eliminated

--- debug-util.js
+++ debug-util.js
@@ -1,6 +1,5 @@
 "use strict";
 const chalk = require('chalk');
-const fs = require('fs');
 const caller = require('./caller');
 
 const date = new Date();
@@ -22,11 +21,6 @@ function debug(msg, type, data, callback) {
 
   // get info about who made the call
   const who = caller();
-
-  // verify if logs directory was create and create if not
-  if (!fs.existsSync('./logs')){
-    fs.mkdirSync('./logs');
-  }
 
   // make sure at least a msg was sent
   if(!msg) {
@@ -106,23 +100,6 @@ function colorAndConsole(msg, time, callback) {
   ${yellowb('File')}: ${yellow(msg.path)}
   Func: ${magenta(msg.func)} | type: ${magenta(msg.ftyp)} | method: ${magenta(msg.meth)}
   ${blueb('Data')}: ${typeof msg.data} ${blue(msg.data)}\n`);
-  // now log to file
-  logTofile(msg, time, callback);
-}
-
-function logTofile(msg, time, callback) {
-  // get msg in a nice format
-  const logMsg =
-    `${time}
-${msg.type}: ${msg.mesg}
-'Line': ${msg.line}
-'File': ${msg.path}
-Func: ${msg.func} | type: ${msg.ftyp} | method: ${msg.meth}
-'Data': ${typeof msg.data} ${msg.data}\n`;
-  const file = `log_${date.getMonth() + 1}_${date.getDate()}_${date.getFullYear()}_${date.getHours()}.log`;
-  const title = `================ Log of ${date.getMonth() + 1} ${date.getDate()} ${date.getFullYear()} Hour: ${date.getHours()} =====`;
-  // append file
-  fs.appendFileSync(`./logs/${file}`, `\n\n${logMsg.trim()}`);
   // return one for success
   if (callback)
     return callback(1);
@@ -130,13 +107,13 @@ Func: ${msg.func} | type: ${msg.ftyp} | method: ${msg.meth}
     return 1;
 }
 
+
 // this will disable any console.log on the page
 if (!process.env.DEBUG) {
   console.log = () => {};
 }
 
 module.exports.debug = debug;
-module.exports.logTofile = logTofile;
 module.exports.colorAndConsole = colorAndConsole;
 module.exports.grab = grab;
 module.exports.process = proc;
